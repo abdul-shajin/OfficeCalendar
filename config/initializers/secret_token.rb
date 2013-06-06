78cb7eb001f2eb9c39e99ddaf7b7189bba4c1304
Shajin <shajinvk@gmail.com>
2013-06-06 08:57:58 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeCalendar::Application.config.secret_token = '42a4ac62d53e0c465b7183f6babda31a698326966f02cdd14af200c73bbfca963734aa86577b51f4f3b9f0af1dc766388071e225aa72ac277783be0e76e842c2'
