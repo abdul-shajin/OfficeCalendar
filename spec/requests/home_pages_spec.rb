12a61ba1d2939482007c6f9d99b69a4eaa7319e5
Shajin <shajinvk@gmail.com>
2013-06-06 10:33:08 +0530

    home page

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 describe "HomePages" do
   subject { page }
-  before { visit '/' }
   describe 'root page' do
-    it 'should have right title' do
-      it { should have_selector('title', :text => 'Office Calendar') }
-    end
+    before { visit root_path }
+    it { should have_selector('title', :text => 'Office Calendar') }
   end
 end
