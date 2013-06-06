1cab5355d2876f7fd405890721336f1d0d24dccd
Shajin <shajinvk@gmail.com>
2013-06-06 14:04:11 +0530

    basic calendar display - finished

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 describe "HomePages" do
   subject { page }
-  describe 'root page' do
+  describe 'root page', :js => true do
     before { visit root_path }
     it { should have_selector('title', :text => 'Office Calendar') }
     it { should have_content('Welcome to Office Calendar')}
+    it { should have_content(Date.today.strftime('%B'))}
   end
 end
