6423e5fc75513f07d74a8b82cc21a9fca5d6bb81
Shajin <shajinvk@gmail.com>
2013-06-06 19:42:19 +0530

    validations

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,3 +1,4 @@
 class Holiday < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :date, :name
+
 end
--- spec/models/holiday_spec.rb
+++ spec/models/holiday_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Holiday do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a holiday date" do
+    Holiday.create(:name => 'random name').should_not be_valid
+  end
+
+  it "is invalid without a holiday name" do
+    Holiday.create(:date => Date.today).should_not be_valid
+  end
+
 end
