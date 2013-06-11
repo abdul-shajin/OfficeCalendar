ee9a5c8a8b05a19058543fc75c561087483018c6
Shajin <shajinvk@gmail.com>
2013-06-11 20:59:28 +0530

    engine testing

---
+++ blorgh/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '2bff3601977a712526e8abcfa64674f5b15e526325685b09023044f2566813467ccdf9ffd6f8e793bfd944d3e29849e395394ba2afb3c5899ac9d821bca7b26d'
